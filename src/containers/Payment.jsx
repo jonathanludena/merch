dd605ccfec7bf4276aa704087cb156f8a6b5dfb4
jonathanludena <jonathanludenai@gmail.com>
2021-05-26 14:51:26 -0500

    Boton de pago con API Paypal

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,73 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PayPalButton } from "react-paypal-button";
+import { useHistory } from "react-router-dom";
 
+import AppContext from "../context/AppContext";
 import "../styles/components/Payment.css";
 
 const Payment = () => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+
+  const history = useHistory();
+
+  const paypalOptions = {
+    clientId:
+      "AWsWRniK5QvOz68UEHZ7WGyUZQGtFXwRQzQu-_FzN4Eog_I9vP74LN5qjYFU7yf6sC5Hh57W5ezhDau1",
+    intent: "capture",
+    currency: "USD",
+  };
+
+  const buttonStyles = {
+    layout: "vertical",
+    shape: "rect",
+  };
+
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if (data.status === "COMPLETED") {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data,
+      };
+
+      addNewOrder(newOrder);
+      history.push("/checkout/success");
+    }
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del Pedido</h3>
-        <div className="Payment-button">Boton de Pago con PayPal</div>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>$ {item.price}</span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-button">
+          <PayPalButton
+            paypalOptions={paypalOptions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal()}
+            onPaymentStart={() => console.log("Start Payment")}
+            onPaymentSuccess={(data) => handlePaymentSuccess(data)}
+            onPaymentError={(error) => console.error(error)}
+            onPaymentCancel={(data) => console.log(data)}
+          />
+        </div>
       </div>
       <div></div>
     </div>
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -26,11 +26,19 @@ const useInitialState = () => {
     });
   };
 
+  const addNewOrder = (payload) => {
+    setState({
+      ...state,
+      orders: [...state.orders, payload],
+    });
+  };
+
   return {
     addToCart,
     removeToCart,
     state,
     addToBuyer,
+    addNewOrder,
   };
 };
 
