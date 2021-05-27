fa203e9e27e17e3e8a28b321481b4398b078479a
jonathanludena <jonathanludenai@gmail.com>
2021-05-27 09:35:17 -0500

    Integracion de API Maps JS

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,18 +1,22 @@
 import React, { useContext } from "react";
 import AppContext from "../context/AppContext";
 
+import Map from "../components/Map";
 import "../styles/components/Success.css";
 
 const Success = () => {
-  const { state } = useContext(AppContext);
-  const { buyer } = state;
+  const {
+    state: { buyer },
+  } = useContext(AppContext);
 
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{buyer.name}, Gracias por tu compra</h2>
-        <span>Tu pedido llegara en 3 dias a tu direccion: {buyer.address}</span>
-        <div className="Success-map">Google Maps</div>
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <span>{`Tu pedido llegara en 3 dias a tu direccion: ${buyer.address}`}</span>
+        <div className="Success-map">
+          <Map />
+        </div>
       </div>
     </div>
   );
